0c295ba40da937e3128948670a78c1b886ea60a3
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-07-05 10:49:41 +0530

    changed from table to div

--- drawing.js
+++ drawing.js
@@ -7,4 +7,5 @@ const drawPlayer = (player) => {
   const { currentPosition, symbol } = player.getInfo();
   const element = document.getElementById(currentPosition);
   element.innerHTML = symbol;
+
 };
